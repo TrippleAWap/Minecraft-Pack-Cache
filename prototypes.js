bed398c38f9e932c0973f260232d61c75f415175
TrippleAWap <90356816+TrippleAWap@users.noreply.github.com>
2023-09-10 15:04:28 -0600

    Create prototypes.js

---
+++ prototypes.js
@@ -0,0 +1,55 @@
+import {Player, world} from "@minecraft/server";
+
+let scoreboards = {}
+Object.defineProperties(Player.prototype, {
+    distanceTo: {
+        value: function (x,y,z) {
+            const { x: px, y: py, z: pz } = this.location
+            return Math.sqrt((x - px) ** 2 + (y - py) ** 2 + (z - pz) ** 2)
+        }
+    },
+    score: {
+        get() {
+            /* @type {Player} */
+            const player = this;
+            return new Proxy({}, {
+                get(target, key) {
+                    try {
+                        return (scoreboards[key] ??= world.scoreboard.getObjective(key)).getScore(player) ?? 0;
+                    } catch {
+                        return 0;
+                    }
+                },
+                set(target, key, value) {
+                    try {
+                        (scoreboards[key] ??= world.scoreboard.getObjective(key)).setScore(player, value)
+                    } catch {
+                        player.runCommandAsync(`scoreboard players set @s ${key} ${value}`)
+                    }
+                    return true;
+                },
+            })
+        },
+    },
+    blockLocation: {
+        get() {
+            const vals = ['x', 'y', 'z']
+            return Object.values(this.location).reduce((prev, curr, i) => prev[vals[i]] = Math.floor(curr), {}) // returns {x: 0, y: 0, z: 0}
+        }
+    }
+})
+Object.defineProperties(Number.prototype, {
+    short: {
+        get: function () {
+            const number = this.valueOf();
+            if (number === 0) return "0";
+
+            const suffixes = ["", "k", "m", "b", "t", "q", "Q", "s", "S", "o", "n", "d", "U", "D", "T", "Qt", "Qd", "Sd", "Od", "Nd", "V", "Uv", "Dv"];
+            const power = Math.floor(Math.log10(Math.abs(number)) / 3);
+            const sign = number < 0 ? "-" : "";
+
+            return sign + (Math.abs(number) / Math.pow(10, power * 3)).toFixed(2) + (suffixes[power] || "");
+        }
+    }
+});
+
